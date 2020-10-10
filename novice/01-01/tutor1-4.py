101c53b09534663253eb91d13649cf6b703bfa79
Iklimah-unu27 <72369546+Iklimah-unu27@users.noreply.github.com>
2020-10-10 21:31:50 +0700

    Add files via upload

---
+++ novice/01-01/tutor1-4.py
@@ -0,0 +1,48 @@
+#interactive mode (if)
+you_are_awesome = True
+if you_are_awesome :
+    print("just do what you love!")
+
+#using python as a calculator 
+#numbers
+print (2 + 3)
+#output 5
+print (50 - 5 * 6) 
+#output 20
+print ((50 - 5 * 6) / 4)
+#output 5.0
+print (8 / 5)
+#output 1.6
+print (17 / 3)
+#output 5.666666666666667
+print (17 // 3)
+#output 5
+print (17 % 3)
+#output 2
+print (5 * 3 +2)
+#output 17
+
+#python use the ** operator
+print (5 ** 2)
+#output 25
+print (2 ** 7)
+#output 128
+
+#String
+print ("spam eggs")
+print ("doesn't") 
+print ("Isn\'t," "they said.")
+print ("3 times 'un', followed by 'ium")
+print ("Py""thon")
+
+#Lists
+squares = [1, 4, 9, 16, 25]
+print (squares)
+print ( squares[0] )
+print ( squares [4]) 
+print (squares [:])
+print (squares + [36, 49, 64, 81, 100])
+
+#First Steps Towards Programming
+i = 256*256
+print("The value of i is", i)
