5405b795cbc8271aa2afe429a059e7636f6f0325
Iklimah <iklimah@pop-os.localdomain>
2020-10-12 15:36:29 +0700

    no2hari2.py

---
+++ novice/02-10/no2hari2.md
@@ -0,0 +1,9 @@
+Hasil dari file Python-tutorial.py sebagai berikut
+
+/usr/bin/python3 /home/iklimah/Documents/praxis-academy/novice/02-10/Python-tutorial.py
+
+hee
+u
+aku sangat suka anggrek
+kita sangat rajin dia sanaget"pandai!"
+cerintanya si ina, ini, and itu.
---
+++ novice/02-10/no2hari2.py
@@ -0,0 +1,35 @@
+def fib(n):    
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+def fib2(n):   
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+#output
+
+#Input and Output
+u = 'hee'
+print (u)
+repr(u)
+str(1/7)
+'0.14285714285714285'
+x = 10 * 3.25
+y = 200 * 200
+u = 'The value of x is ' + repr(x) + ', and y is ' + repr(y) + '...'
+print('u')
+
+#contoh string
+tumbuhan = 'anggrek'
+print(f'aku sangat suka {tumbuhan}')
+
+#con metode string format ()
+print('kita sangat {} dia sanaget"{}!"'.format('rajin', 'pandai'))
+
+print('cerintanya si {0}, {1}, and {other}.'.format('ina', 'ini', other='itu'))
