7306f202d4d94723233e181c598aa606f6fd7f96
Iklimah <iqlima.safira99@gmail.com>
2020-11-03 09:19:52 +0700

    db.sqlite3

--- novice/03-02/fish/penjual/views.py
+++ novice/03-02/fish/penjual/views.py
@@ -22,21 +22,4 @@ def detail(req, id):
     return render(req, 'detail.html',
         { 'data': seller,
         })
-
-# def edit(req, id):
-#     if req.POST:
-#         pembeli = models.pembeli.objects.filter(pk=id).update(
-#             nama_pmbli=req.POST['nama_pmbli'],
-#             alamat=req.POST['alamat'],
-#             total_beli=req.POST['total_beli'])  
-#         return redirect('/')
-
-#     buyer = models.pembeli.objects.filter(pk=id).first()
-#     return render(req, 'edit.html',
-#     {
-#         'data': buyer,
-#         })  
-
-# def delete(req, id):
-#     models.pembeli.objects.filter(pk=id).delete()
-#     return redirect('/')
+        
